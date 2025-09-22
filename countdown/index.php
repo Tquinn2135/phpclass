017c159145d5ed2c65fa7f5076b352587c471a01
Tom Quinn <Tquinn2135@gmail.com>
2025-09-22 06:31:52 -0500

    Added end of semester timer

---
+++ countdown/index.php
@@ -0,0 +1,63 @@
+<?php
+$secPerMin = 60;
+$secPerhour = 60 * $secPerMin;
+$secPerDay = 24 * $secPerhour;
+$secPerYear = 365 * $secPerDay;
+
+$now = time();
+$burningMan = mktime(12, 0, 0,8, 25,2033);
+
+
+//Number in seconds
+$seconds = $burningMan - $now;
+
+
+//years
+$years = floor($seconds/$secPerYear);
+$seconds = $seconds - ($years * $secPerYear);
+
+$days = floor($seconds/$secPerDay);
+$seconds = $seconds - ($days * $secPerDay);
+
+$hours = floor($seconds/$secPerhour);
+$seconds = $seconds - ($hours*$secPerhour);
+
+$minutes = floor($seconds / $secPerMin);
+$seconds = $seconds - ($minutes *$secPerMin)
+
+?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tom's Website</title>
+    <link rel="stylesheet" href="/css/base.css">
+</head>
+<body>
+<?php
+include "../includes/header.php"
+?>
+<div id="three-column">
+    <?php
+    include "../includes/navigation.php"
+    ?>
+    <main>
+        <h3>Countdown to burning man</h3>
+        <span>Years:<?=$years?></span>
+        <span>Days:<?=$days?></span>
+        <span>Hours:<?=$hours?></span>
+        <span>Minutes:<?=$minutes?></span>
+        <span>Seconds:<?=$seconds?></span>
+    </main>
+</div>
+<?php
+include "../includes/footer.php"
+?>
+</body>
+</html>
+
+
+
+
